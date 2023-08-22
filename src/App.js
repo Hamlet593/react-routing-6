a1f0a1e480517e76e86ae5eb775fdaf020477e16
Hamo Muradyan <hamlet@advancedtech.am>
2023-08-22 14:17:22 +0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import BlogPage from "./pages/BlogPage";
+import Homepage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/about">About</Link>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/posts" element={<BlogPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,9 @@
+const BlogPage = () => {
+  return (
+    <div>
+      <h2>This is our blog page</h2>
+    </div>
+  );
+};
+
+export default BlogPage;
