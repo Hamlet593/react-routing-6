47b5aa6e429408acc13ed6e96ebeba84800c286b
Hamo Muradyan <hamlet@advancedtech.am>
2023-08-24 15:18:51 +0400

    add special css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,20 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const setActive = ({ isActive }) => (isActive ? "active-link" : "");
+
   return (
     <>
       <header>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/about">About</Link>
+        <NavLink to="/" className={setActive}>
+          Home
+        </NavLink>
+        <NavLink to="/posts" className={setActive}>
+          Posts
+        </NavLink>
+        <NavLink to="/about" className={setActive}>
+          About
+        </NavLink>
       </header>
       <main className="container">
         <Outlet />
